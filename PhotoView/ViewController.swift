10d4e68c00bddbd96f8f0b1686b408ffa22fc184
Macintosh <macintosh@macintoshs-mac-mini.local>
2016-04-09 12:55:17 -0400

    final demo

--- PhotoView/ViewController.swift
+++ PhotoView/ViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import MapKit
-import CoreLocation
 import Photos
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MKMapViewDelegate{
@@ -17,31 +16,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet weak var mapView: MKMapView!
     
-    var annotation: MKAnnotation!
-    
+   
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
         imagePicker.delegate = self
-//        mapView.delegate = self
-//        let longPress = UILongPressGestureRecognizer(target: self, action: "mapAction:")
-//        longPress.minimumPressDuration = 1.0
-//        self.mapView.addGestureRecognizer(longPress)
-        
-    }
-/*
-    func mapAction(gestureRecognizer:UIGestureRecognizer) {
-        let touchPoint = gestureRecognizer.locationInView(self.mapView)
-        var newCoord:CLLocationCoordinate2D = mapView.convertPoint(touchPoint, toCoordinateFromView: self.mapView)
-        print("Gesture recognized")
-   
-        
-        /*        */
+        mapView.delegate = self
+     
     }
 
-*/
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -49,79 +34,43 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 
     @IBAction func loadImageButtonTapped(sender: AnyObject) {
-//            imagePicker.allowsEditing = false
+
             imagePicker.sourceType = .PhotoLibrary
-            
             presentViewController(imagePicker, animated: true, completion: nil)
     }
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
        
-  //      if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-  //          imageView.contentMode = .ScaleAspectFit
-  //          imageView.image = pickedImage
-        
-            imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
-        
-        /*For showing on the map
+
+            imageView.image =  info[UIImagePickerControllerOriginalImage] as? UIImage
+
+           //For showing on the map, get the location
             let url: NSURL = info[UIImagePickerControllerReferenceURL] as! NSURL
             let phAssetResults = PHAsset.fetchAssetsWithALAssetURLs([url], options:nil)
             let asset = phAssetResults.firstObject
             let latitude = asset!.location!!.coordinate.latitude
             let longitude = asset!.location!!.coordinate.longitude
- 
+          //create a pin and show on the map
+            //default zoom for map
             let theSpan:MKCoordinateSpan = MKCoordinateSpanMake(0.1 , 0.1)
+            //get the location
             let location = CLLocationCoordinate2DMake(latitude, longitude)
+            //map will be show the region around our location
             let theRegion:MKCoordinateRegion = MKCoordinateRegionMake(location, theSpan)
             self.mapView.setRegion(theRegion, animated: true)
-            
+        
+            //create a dropped pin
             let annotation = MKPointAnnotation()
             annotation.coordinate = location
             annotation.title = "My Favorite Place"
-            //annotation.subtitle = "Home of App Inspire"
             self.mapView.addAnnotation(annotation)
-            
-            
-            print("\(latitude), \(longitude)")
-          */
-            
-            /*
-            let library = ALAssetsLibrary()
-            library.assetForURL(url, resultBlock: { (asset: ALAsset!) in
-                if asset.valueForProperty(ALAssetPropertyLocation) != nil {
-                    let latitude = (asset.valueForProperty(ALAssetPropertyLocation) as! CLLocation!).coordinate.latitude
-                    let longitude = (asset.valueForProperty(ALAssetPropertyLocation) as! CLLocation!).coordinate.longitude
-                    print("\(latitude), \(longitude)")
-                    
-
-                    
-                    let theSpan:MKCoordinateSpan = MKCoordinateSpanMake(0.1 , 0.1)
-                    let location = CLLocationCoordinate2DMake(latitude, longitude)
-                    let theRegion:MKCoordinateRegion = MKCoordinateRegionMake(location, theSpan)
-                    self.mapView.setRegion(theRegion, animated: true)
-
-                    var annotation = MKPointAnnotation()
-                    annotation.coordinate = location
-                    annotation.title = "My Favorite Place"
-                    //annotation.subtitle = "Home of App Inspire"
-                    self.mapView.addAnnotation(annotation)
-                }
-                },
-                failureBlock: { (error: NSError!) in
-                    print(error.localizedDescription)
-            })
-            */
-  //      }
-
-
-  
         
-        dismissViewControllerAnimated(true, completion: nil)
+            dismissViewControllerAnimated(true, completion: nil)
     }
     
 
     func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-        print("removing annotation")
+
         if let annotation = view.annotation as? MKPointAnnotation {
             let latitude = annotation.coordinate.latitude
             let longitude = annotation.coordinate.longitude
@@ -139,8 +88,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
        
         let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "myPin")
         annotationView.canShowCallout = true
-        let btn2 = UIButton(type: .DetailDisclosure)
-        annotationView.leftCalloutAccessoryView = btn2
+        let btn = UIButton(type: .DetailDisclosure)
+        annotationView.leftCalloutAccessoryView = btn
         return annotationView
     }
 
